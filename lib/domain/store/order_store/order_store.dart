dc79188d95f57091b75646209f588651940658ba
Spartak Maznytskyi <109909363+Spartakm67@users.noreply.github.com>
2024-12-26 22:21:45 +0200

    Time functions in OrderStore fixed

--- lib/domain/store/order_store/order_store.dart
+++ lib/domain/store/order_store/order_store.dart
@@ -30,28 +30,28 @@ abstract class OrderStoreBase with Store {
   //   },
   // ).skip(1).toList();
 
-  List<TimeOfDay> availableTimes = List.generate(
-    23,
-        (index) {
-      final now = DateTime.now();
-      int hours = 9 + (index ~/ 2);
-      int minutes = (index % 2 == 0) ? 0 : 30;
-
-      if (now.hour > hours || (now.hour == hours && now.minute > minutes)) {
-        if (minutes == 0) {
-          minutes = 30;
-        } else {
-          hours += 1;
-          minutes = 0;
-        }
-      }
-      DateTime generatedTime = DateTime(now.year, now.month, now.day, hours, minutes);
-
-      return generatedTime.isAfter(now)
-          ? TimeOfDay(hour: hours, minute: minutes)
-          : null;
-    },
-  ).whereType<TimeOfDay>().toList();
+  // List<TimeOfDay> availableTimes = List.generate(
+  //   23,
+  //       (index) {
+  //     final now = DateTime.now();
+  //     int hours = 9 + (index ~/ 2);
+  //     int minutes = (index % 2 == 0) ? 0 : 30;
+  //
+  //     if (now.hour > hours || (now.hour == hours && now.minute > minutes)) {
+  //       if (minutes == 0) {
+  //         minutes = 30;
+  //       } else {
+  //         hours += 1;
+  //         minutes = 0;
+  //       }
+  //     }
+  //     DateTime generatedTime = DateTime(now.year, now.month, now.day, hours, minutes);
+  //
+  //     return generatedTime.isAfter(now)
+  //         ? TimeOfDay(hour: hours, minute: minutes)
+  //         : null;
+  //   },
+  // ).whereType<TimeOfDay>().toList();
 
 
   // List<TimeOfDay> availablePointTimes = List.generate(
@@ -64,17 +64,66 @@ abstract class OrderStoreBase with Store {
   //   },
   // ).skip(1).toList();
 
+  // List<TimeOfDay> availablePointTimes = List.generate(
+  //     44,
+  //         (index) {
+  //       final now = DateTime.now();
+  //       int totalMinutes = 9 * 60 + 15 + index * 15;
+  //       int hours = totalMinutes ~/ 60;
+  //       int minutes = totalMinutes % 60;
+  //       DateTime generatedTime = DateTime(now.year, now.month, now.day, hours, minutes);
+  //       return generatedTime.isAfter(now) ? TimeOfDay(hour: hours, minute: minutes) : null;
+  //     },
+  //   ).whereType<TimeOfDay>().toList();
+
+
+  List<TimeOfDay> availableTimes = List.generate(
+    23,
+        (index) {
+      final now = DateTime.now();
+      int hours = 9 + (index ~/ 2);
+      int minutes = (index % 2 == 0) ? 0 : 30;
+
+      // Генерація часу
+      DateTime generatedTime = DateTime(now.year, now.month, now.day, hours, minutes);
+
+      // Перевірка на неробочий час
+      if (now.hour >= 20 || now.hour < 9) {
+        // Якщо зараз неробочий час, генеруємо час з 9:00
+        if (hours < 20) {
+          return TimeOfDay(hour: hours, minute: minutes);
+        }
+      } else if (generatedTime.isAfter(now)) {
+        return TimeOfDay(hour: hours, minute: minutes);
+      }
+      return null;
+    },
+  ).whereType<TimeOfDay>().toList();
+
   List<TimeOfDay> availablePointTimes = List.generate(
-      44,
-          (index) {
-        final now = DateTime.now();
-        int totalMinutes = 9 * 60 + 15 + index * 15;
-        int hours = totalMinutes ~/ 60;
-        int minutes = totalMinutes % 60;
-        DateTime generatedTime = DateTime(now.year, now.month, now.day, hours, minutes);
-        return generatedTime.isAfter(now) ? TimeOfDay(hour: hours, minute: minutes) : null;
-      },
-    ).whereType<TimeOfDay>().toList();
+    44,
+        (index) {
+      final now = DateTime.now();
+      int totalMinutes = 9 * 60 + 15 + index * 15;
+      int hours = totalMinutes ~/ 60;
+      int minutes = totalMinutes % 60;
+
+      // Генерація часу
+      DateTime generatedTime = DateTime(now.year, now.month, now.day, hours, minutes);
+
+      // Перевірка на неробочий час
+      if (now.hour >= 20 || now.hour < 9) {
+        // Якщо зараз неробочий час, генеруємо час з 9:00
+        if (hours < 20) {
+          return TimeOfDay(hour: hours, minute: minutes);
+        }
+      } else if (generatedTime.isAfter(now)) {
+        return TimeOfDay(hour: hours, minute: minutes);
+      }
+      return null;
+    },
+  ).whereType<TimeOfDay>().toList();
+
 
   @action
   void selectTime(TimeOfDay time) {
