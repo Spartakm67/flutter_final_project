1f0aae4859ebd27306a604abdee807cd885e1f57
Spartakm67 <spartakm@ukr.net>
2025-03-06 23:03:57 +0200

    ErrorScreen added

--- lib/presentation/screens/auth/error_app.dart
+++ lib/presentation/screens/auth/error_app.dart
@@ -20,30 +20,64 @@ class ErrorApp extends StatelessWidget {
   }
 }
 
+// class ErrorApp extends StatelessWidget {
+//   final String message;
+//   final VoidCallback onRetry;
 //
-// class ErrorScreen extends StatelessWidget {
-//   final String errorMessage;
-//
-//   const ErrorScreen({super.key, required this.errorMessage});
+//   const ErrorApp({super.key, required this.message, required this.onRetry});
 //
 //   @override
 //   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: Center(
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: [
-//             const Icon(Icons.error, size: 50, color: Colors.red),
-//             const SizedBox(height: 20),
-//             const Text(
-//               'Initialization Failed',
-//               style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.red),
+//     return MaterialApp(
+//       home: Scaffold(
+//         body: Center(
+//           child: Padding(
+//             padding: const EdgeInsets.all(20.0),
+//             child: Column(
+//               mainAxisAlignment: MainAxisAlignment.center,
+//               children: [
+//                 Image.asset(
+//                   'assets/images/error.jpg',
+//                   width: 150,
+//                   height: 150,
+//                 ),
+//                 const SizedBox(height: 20),
+//                 Text(
+//                   message,
+//                   style: const TextStyle(fontSize: 18, color: Colors.red),
+//                   textAlign: TextAlign.center,
+//                 ),
+//                 const SizedBox(height: 20),
+//                 ElevatedButton(
+//                   onPressed: onRetry,
+//                   child: const Text('Спробувати знову'),
+//                 ),
+//               ],
 //             ),
-//             const SizedBox(height: 10),
-//             Text(errorMessage, textAlign: TextAlign.center),
-//           ],
+//           ),
 //         ),
 //       ),
 //     );
 //   }
+// }
+
+
+// Widget build(BuildContext context) {
+//   return FutureBuilder(
+//     future: widget.store.loadData(), // Ваш метод отримання даних
+//     builder: (context, snapshot) {
+//       if (snapshot.connectionState == ConnectionState.waiting) {
+//         return const Center(child: CircularProgressIndicator());
+//       } else if (snapshot.hasError) {
+//         return ErrorApp(
+//           message: 'Помилка завантаження даних',
+//           onRetry: () {
+//             setState(() {}); // Перезавантаження екрану
+//           },
+//         );
+//       } else {
+//         return YourHomeScreenContent(); // Основний контент
+//       }
+//     },
+//   );
 // }
